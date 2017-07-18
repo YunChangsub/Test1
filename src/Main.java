2e73f56a55883182ec882799b530eec3a1882f3b
YunChangsub <ckdwl903a@naver.com>
2017-07-18 18:25:27 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
-		
 		Scanner scanner = new Scanner(System.in);
 		String text = scanner.nextLine();
 		
@@ -35,6 +33,6 @@ public class Main
 			if (j == ar[i].length()) 
 				num++;
 		}
-		System.out.println("\nÀüÃ¼ ¼ö : " + num);
+		System.out.println("\nÃ€Ã¼ÃƒÂ¼ Â¼Ã¶ : " + num);
 	}
 }
